57499c11da5e8d2ab51f6e6248e752148f8dcaa8
DuyThanh <thanhnd2512@gmail.com>
2022-06-26 21:46:11 +0700

    xu ly input xong

--- Controller/FileIO.py
+++ Controller/FileIO.py
@@ -1,23 +1,153 @@
+from numpy import number
+from FinancialLeverage import *
+import array as arr
 
-class fileIO:
-    def __init__(self) -> None:
-        pass
+def createNewExcelFile():
+    file = open("Data.csv",mode="r",encoding="utf-8-sig")
+    file_new = open("Data_new.csv", mode="w",encoding="utf-8-sig")
 
-    def readToFile(self):
-        file = open('../assets/tempIO/Book1.csv',mode="r",encoding="utf-8-sig")
+    header = file.readline()
 
-        header = file.readline()
+    file_new.write(header.strip() + ",Chênh lệch" + ",Phần trăm\n")
 
-        row = file.readline()
+    row = file.readline()
+    while row != "":
         row_list = row.split(",")
 
-        Data_2020 = row_list[1]
-        Data_2019 = row_list[2]   
+        last_year = float(row_list[2])
+        this_year = float(row_list[1])
+
+        chenhlech = this_year - last_year
+        chenhlech1 = ((this_year - last_year)/last_year) * 100
+        # bổ sung
+
+        Round = round(chenhlech1,2)
+
+        row_new = row.strip() + "," + str(chenhlech)+ "," + str(Round)+"\n"
         # print(row_list)
-        print(row_list)
-    pass
+
+        file_new.write(row_new)
+        row = file.readline()
+    file.close()
+    file_new.close()
 
 
-Thanh = fileIO()
+def tinhtoan():
+    file = open("Data_new.csv",mode="r",encoding="utf-8-sig")
+    file_new = open("DataFinal.csv", mode="w",encoding="utf-8-sig")
+    header = file.readline()
+    row = file.readline()
+    flag = 0
+    data1 = 0
+    data2 = 0
+    data3 = 0
+    data4 = 0
+    data5 = 0
+    data6 = 0
+    data7 = 0
+    data8 = 0
+    data9 = 0
+    data10 = 0
+    data11 = 0
+    data12 = 0
+    data13 = 0
+    data14 = 0
+    data15 = 0
+    data16 = 0
+    while row != "":
+        row_list = row.split(",")
+        c1 = int(row_list[1])
+        c2 = int(row_list[2])
+        c3 = float(row_list[3])
+        c4 = float(row_list[4])
 
-Thanh.readToFile()
+        if(flag <= 4):
+            a = arr.array('d',[c1])
+            b = arr.array('d',[c2])
+            c = arr.array('d',[c3])
+            d = arr.array('d',[c4])
+            data1 = round(data1 + a[0],3) 
+            data2 = round(data2 + b[0],3)
+            data3 = round(data3 + c[0],3)
+            data4 = round(data4 + d[0],3)
+            
+            # print("thanh dep trai") 
+        elif(4 < flag <= 10):
+            a = arr.array('d',[c1])
+            b = arr.array('d',[c2])
+            c = arr.array('d',[c3])
+            d = arr.array('d',[c4])
+
+            data5 = round(data5 + a[0],3)
+            data6 = round(data6 + b[0],3)
+            data7 = round(data7 + c[0],3)
+            data8 = round(data8 + d[0],3)
+            print(data5)
+        elif(10 < flag <=12):
+            a = arr.array('d',[c1])
+            b = arr.array('d',[c2])
+            c = arr.array('d',[c3])
+            d = arr.array('d',[c4])
+
+            data9 = round(data9 + a[0],3)
+            data10 = round(data10 + b[0],3)
+            data11 = round(data11 + c[0],3)
+            data12 = round(data12 + d[0],3)
+        else:
+            a = arr.array('d',[c1])
+            b = arr.array('d',[c2])
+            c = arr.array('d',[c3])
+            d = arr.array('d',[c4])
+
+            data13 = round(data13 + a[0],3)
+            data14 = round(data14 + b[0],3)
+            data15 = round(data15 + c[0],3)
+            data16 = round(data16 + d[0],3)
+
+
+        flag += 1
+        row = file.readline()
+
+    row_new = row.strip() + "Tài sản ngắn hạn" + "," + str(data1)+ "," + str(data2)+ "," + str(data3)+ "," + str(data4)+"\n"
+    file_new.write(row_new)
+
+    row_new1 = row.strip() + "Tài sản dài hạn" + "," + str(data5)+ "," + str(data6)+ "," + str(data7)+ "," + str(data8)+"\n"
+    file_new.write(row_new1)
+
+    row_new2 = row.strip() + "Tổng cộng tài sản" + "," + str(data5+data1)+ "," + str(data6+data2)+ "," + str(data7+data3)+ "," + str(data8+data4)+"\n"
+    file_new.write(row_new2)
+
+    row_new3 = row.strip() + "Nợ phải trả" + "," + str(data9)+ "," + str(data10)+ "," + str(data11)+ "," + str(data12)+"\n" 
+    file_new.write(row_new3)
+
+    row_new4 = row.strip() + "Nợ phải trả" + "," + str(data9-data13)+ "," + str(data10-data14)+ "," + str(data11-data15)+ "," + str(data12-data16)+"\n"  
+    file_new.write(row_new4)  
+
+
+        
+
+    # data = tai_san_ngan_han(row1,row2,row3,row4,row5)
+
+    # data1 = tai_san_dai_han(row6,row7,row8,row9,row10,row11)
+
+    # data2 = tong_cong_tai_san(data,data1)
+
+    # data3 = no_phai_tra(row12,row13)
+
+    # data4 = tong_nguon_von(row12,row13,row14)
+
+    # print(data)
+    # print(data1)
+    # print(data2)
+    # print(data3)
+    # print(data4)
+    
+
+    
+
+    
+    
+
+if __name__ == "__main__":
+    start = tinhtoan()
+
---
+++ Controller/FinancialLeverage.py
@@ -0,0 +1,21 @@
+
+
+def tai_san_ngan_han(so1,so2,so3,so4,so5):
+    return so1+so2+so3+so4+so5
+
+def tai_san_dai_han(so6,so7,so8,so9,so10,so11):
+    return so6+so7+so8+so9+so10+so11
+
+def tong_cong_tai_san(ngan_han,dai_han):
+    return ngan_han+dai_han
+
+def no_phai_tra(so12,so13):
+    return so12+so13
+
+def tong_nguon_von(so12,so13,so14):
+    return (so12+so13)-so14
+  
+
+
+
+
